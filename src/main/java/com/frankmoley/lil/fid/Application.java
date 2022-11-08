0c83683667155058caf32a612d632ed92ce12ca0
Thiago Emmerich <31050830+thiemmerich@users.noreply.github.com>
2022-11-08 14:46:47 -0300

    Finished the course

--- src/main/java/com/frankmoley/lil/fid/Application.java
+++ src/main/java/com/frankmoley/lil/fid/Application.java
@@ -1,9 +1,7 @@
 package com.frankmoley.lil.fid;
 
 import com.frankmoley.lil.fid.config.ApplicationConfig;
-import com.frankmoley.lil.fid.service.GreetingService;
 import com.frankmoley.lil.fid.service.OutputService;
-import com.frankmoley.lil.fid.service.TimeService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -13,9 +11,10 @@ public class Application {
         ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
         OutputService outputService = context.getBean(OutputService.class);
 
-        for (int i=0;i<5;i++){
+        for (int i = 0; i < 5; i++) {
             outputService.generateOutput();
-            Thread.sleep(5000);
+            Thread.sleep(1000);
         }
     }
+
 }
--- src/main/java/com/frankmoley/lil/fid/service/GreetingService.java
+++ src/main/java/com/frankmoley/lil/fid/service/GreetingService.java
@@ -1,15 +1,23 @@
 package com.frankmoley.lil.fid.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.frankmoley.lil.fid.aspect.Countable;
+import com.frankmoley.lil.fid.aspect.Loggable;
+
+@Service
 public class GreetingService {
 
-    private final String greeting;
+    @Value("${app.greeting}")
+    private String greeting;
 
-    public GreetingService(String greeting){
+    public GreetingService() {
         super();
-        this.greeting = greeting;
     }
 
-    public String getGreeting(String name){
+    @Loggable
+    public String getGreeting(String name) {
         return greeting + " " + name;
     }
 }
